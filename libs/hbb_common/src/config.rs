0798459306bff28008ea36e7002ed466c1da0a7a
cercky <cercky@qq.com>
2024-12-27 15:05:46 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -103,8 +103,8 @@ const CHARS: &[char] = &[
 pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
 pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
 
-pub const RS_SERVERS: &str = match option_env!("RENDEZVOUS_SERVER") {
-    Some(key) if !key.is_empty() => key,
+pub const RS_SERVERS: &[&str] = match option_env!("RENDEZVOUS_SERVER") {
+    Some(key) if !key.is_empty() => &[key],
     _ => RENDEZVOUS_SERVERS,
 };
 
